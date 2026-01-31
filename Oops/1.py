0a4a9081587b9f451bc8eedd10acac498a6b7829
rohittt027 <rohitkaithwaria99gmail@gmail.com>
2026-02-01 00:30:33 +0530

    Oops code commit in github

---
+++ Oops/1.py
@@ -0,0 +1,31 @@
+class Room:
+            num_of_student = 90
+            num_of_teachers = 5
+            num_of_desk = 50
+            num_of_blackboard = 3
+
+my_obj = Room()
+
+print(my_obj.num_of_student)
+print(my_obj.num_of_teachers)
+print(my_obj.num_of_desk)
+print(my_obj.num_of_blackboard)
+
+
+
+
+
+# -----------------------------------------------------------
+
+class Rohit:
+            Degree = "BCA"
+            Address = "Bihar"
+            Email = "rohitkaithwaria99@gmail.com"
+            Bank_Blance = 10000000
+
+my_obj = Rohit()
+
+print(my_obj.Degree)
+print(my_obj.Address)
+print(my_obj.Email)
+print(my_obj.Bank_Blance)
